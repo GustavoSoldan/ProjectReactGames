ae10a99fdb027fc5b1e59636989d64f824bb6091
Gustavo <gustavosoldan9@gmail.com>
2023-05-31 21:22:34 -0300

    tets

--- sitegiovane/src/App.js
+++ sitegiovane/src/App.js
@@ -8,12 +8,13 @@ import './assets/css/normalize.css';
 class App extends Component{
   constructor(props){
     super(props);
-    this.state={
+    this.number={
+
 
     }
   }
 
-  render(){
+  render22(){
     return(
       <>
       <Header></Header>
